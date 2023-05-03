990d0a368002556f6ec57a26080d826f1d7213aa
Md Imran Hossen <freelancerimran81@gmail.com>
2023-05-03 22:57:31 +0600

    Update

--- pages/product-details/[id].js
+++ pages/product-details/[id].js
@@ -2,8 +2,16 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import { AiFillStar } from 'react-icons/ai';
+import { RxMinus } from 'react-icons/rx';
+import { FiPlus } from 'react-icons/fi';
+import { useState } from 'react';
+import img from '../../src/assets/Order/refund.png'
+import social from '../../src/assets/social/social.png'
+import Image from 'next/image';
+
 
 const ProductDetails = () => {
+    const [value, setValue] = useState(1);
     const router = useRouter();
 
     const images = [
@@ -22,6 +30,18 @@ const ProductDetails = () => {
     ];
 
 
+
+    const handleIncreaseValue = () => {
+        setValue(value + 1);
+    }
+
+    const handleDecreaseValue = () => {
+        if (value > 1) {
+            setValue(value - 1);
+        }
+    }
+
+
     return (
         <div className='mid-container mx-auto p-5 mt-5  bg-base-100 rounded '>
             <div className='flex gap-10'>
@@ -33,7 +53,6 @@ const ProductDetails = () => {
                         showNav={false}
                     // showBullets={true}
                     // showThumbnails={false}
-
 
                     />
                 </div>
@@ -51,7 +70,7 @@ const ProductDetails = () => {
                         <p className='text-sm text-neutral mt-[3px]'>(0 reviews)</p>
                     </div>
 
-                    <div className='flex gap-5 items-center mt-5 border-b pb-5'>
+                    <div className='flex gap-5 items-center border-b py-5'>
                         <div>
                             <p className='text-xs text-neutral'>SoldBy:</p>
                             <h1 className='text-sm font-semibold'>Lavish Look</h1>
@@ -60,6 +79,87 @@ const ProductDetails = () => {
                         <div className='flex gap-3'>
                             <button className='bg-secondary px-4 py-2 text-primary text-xs rounded border border-primary'>Message Seller</button>
                             <button className='bg-secondary px-4 py-2 text-primary text-xs rounded border border-primary'>Brand</button>
+                        </div>
+                    </div>
+
+                    <div className='py-5 border-b space-y-2'>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Price : </p>
+                            <strike><h1 className='text-sm font-semibold text-neutral'>৳ 1,500</h1></strike>
+                        </div>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Discount Price : </p>
+                            <h1 className='font-bold text-primary'>৳ 1,500</h1>
+                        </div>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Club Point : </p>
+                            <h1 className='text-xs rounded font-semibold bg-secondary px-2 py-1'>325</h1>
+                        </div>
+                    </div>
+
+                    <div className='border-b py-5'>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Quantity : </p>
+                            <div className='flex items-center gap-4'>
+                                <div
+                                    onClick={handleDecreaseValue}
+                                    className='bg-gray-300 p-1 cursor-pointer h-6 w-6 rounded-full flex justify-center items-center text-xl font-bold select-none'> <RxMinus className='text-black' /> </div>
+
+                                <p>{value}</p>
+
+                                <div
+                                    onClick={handleIncreaseValue}
+                                    className='bg-gray-300 p-1 cursor-pointer h-6 w-6 rounded-full flex justify-center items-center text-xl font-semibold select-none'> <FiPlus className='text-black' /> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Total Price : </p>
+                            <h1 className='font-bold text-primary'>৳ {1500 * value}</h1>
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                        <div className='flex items-center gap-5'>
+                           <button className='btn btn-md btn-secondary text-primary font-bold hover:btn-primary capitalize'> Add To Cart</button>
+                           <button className='btn btn-md btn-primary font-bold capitalize'> Buy Now</button>
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                        <div className='flex items-center gap-5 text-sm font-semibold text-primary'>
+                           <h1 className='cursor-pointer select-none'> Add To Wishlist</h1>
+                           <h1 className='cursor-pointer select-none'> Add To Compare</h1>
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Price : </p>
+                            <div className='flex items-center gap-2'>
+                                <Image
+                                src={img}
+                                alt="Picture of the refund"
+                                width={200}
+                                height={200}
+                                ></Image>
+
+                                <h1 className='cursor-pointer text-primary text-sm font-semibold'>View Policy</h1>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='py-5'>
+                        <div className='flex items-center'>
+                            <p className='text-xs text-neutral w-32'>Share : </p>
+                                <Image
+                                src={social}
+                                alt="Picture of the refund"
+                                width={200}
+                                height={200}
+                                ></Image>
                         </div>
                     </div>
                 </div>

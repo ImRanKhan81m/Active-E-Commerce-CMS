1a90e2450de1a0165edae83d05f5cfa6288c691e
Md Imran Hossen <freelancerimran81@gmail.com>
2023-06-11 11:07:13 +0600

    Done

--- src/Components/Home/CategorySec/BrandSec/BrandItem.js
+++ src/Components/Home/CategorySec/BrandSec/BrandItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import img from '../../../../assets/categories/Rectangle 40 (5).png'
 import { AiOutlineRight } from 'react-icons/ai';
+import Image from 'next/image';
 
 const BrandItem = () => {
     return (
         <div className='flex justify-between items-center gap-6 bg-white rounded p-2 shadow'>
             <div className='w-[30%]'>
-                <img src={'https://i.ibb.co/4jdwpQn/redbull.png'} alt="category" className='w-full'/>
+                <Image 
+                src={'https://i.ibb.co/4jdwpQn/redbull.png'} 
+                height={300}
+                width={100}
+                alt="category" 
+                className='w-full'
+                />
             </div>
             <div className='w-[60%]'>
                 <h1 className='font-semibold'>Woman Clothing & Fashion</h1>
--- src/Components/Home/CategorySec/CategorySec/CategoryItem.js
+++ src/Components/Home/CategorySec/CategorySec/CategoryItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import img from '../../../../assets/categories/Rectangle 40 (5).png'
 import { AiOutlineRight } from 'react-icons/ai';
+import Image from 'next/image';
 
 const CategoryItem = () => {
     return (
         <div className='flex justify-between items-center gap-6 bg-white rounded p-2 shadow'>
             <div className='w-[30%]'>
-                <img src={'https://i.ibb.co/z6bbFnm/Rectangle-80.png'} alt="category" className='w-full'/>
+                <Image 
+                src={'https://i.ibb.co/z6bbFnm/Rectangle-80.png'} 
+                height={300}
+                width={100}
+                alt="category" 
+                className='w-full'
+                />
             </div>
             <div className='w-[60%]'>
                 <h1 className='font-semibold'>Woman Clothing & Fashion</h1>
